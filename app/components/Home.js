95c694932b7a6d8ed0ded3ae77d191eb6dab4cec
iEnder <pkmn2024@gmail.com>
2017-01-09 10:32:55 -0500

    article cards

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const Storycard = require('../components/Storycard');
 
 /*
   Home
@@ -6,9 +7,14 @@ const React = require('react');
 
 class Home extends React.Component {
   render() {
+    let arr = [];
+    for(let i = 0; i <= 48; i++) {
+      arr.push(<Storycard  />);
+    }
     return (
-      <div id="main">
-        <h1></h1>
+      <div id="Home">
+        <Storycard type="main" />
+        {arr}
       </div>
     )
   }
--- app/components/StoryCard.js
+++ app/components/StoryCard.js
@@ -1,7 +1,18 @@
 const React = require('react');
 
-class StoryCard extends React.Component {
-
+class Storycard extends React.Component {
+  render() {
+    return (
+      <div className={`storyCard ${this.props.type == "main" ? "cardMain" : "cardNorm"}`}>
+        <img src="https://placeholdit.imgix.net/~text?txtsize=38&txt=&w=520&h=120" alt="#"/>
+        <span className="storyButton" ><i className="fa fa-arrow-right"></i></span>
+        <div>
+          <p>Nullam euismod laoreet auctor</p>
+          <span><i className="fa fa-ellipsis-v"></i></span>
+        </div>
+      </div>
+    )
+  }
 }
 
-module.exports = StoryCard;
+module.exports = Storycard;
