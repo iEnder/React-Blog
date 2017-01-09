1e39471974df4cf49bb5e4acacaa145deb56301d
iEnder <pkmn2024@gmail.com>
2017-01-09 13:56:41 -0500

    social slider

--- app/components/StoryCard.js
+++ app/components/StoryCard.js
@@ -1,6 +1,24 @@
 const React = require('react');
+const CSSTransisitionGroup = require('react-addons-css-transition-group');
 
 class Storycard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      socialOpen: false
+    }
+
+    this.toggleSocial = this.toggleSocial.bind(this);
+  }
+
+  toggleSocial() {
+    this.state.socialOpen = !this.state.socialOpen;
+    this.setState({
+      socialOpen: this.state.socialOpen
+    });
+  }
+
   render() {
     return (
       <div className={`storyCard ${this.props.type == "main" ? "cardMain" : "cardNorm"}`}>
@@ -8,11 +26,28 @@ class Storycard extends React.Component {
         <span className="storyButton" ><i className="fa fa-arrow-right"></i></span>
         <div>
           <p>Nullam euismod laoreet auctor</p>
-          <span><i className="fa fa-ellipsis-v"></i></span>
+          <span><i onClick={this.toggleSocial} className="fa fa-ellipsis-v"></i></span>
         </div>
-      </div>
-    )
+        <CSSTransisitionGroup
+          transitionName="openSocial"
+          transitionEnterTimeout={400}
+          transitionLeaveTimeout={400}
+          transitionAppearTimeout={400}
+          transitionAppear={true}>
+          {this.state.socialOpen ?
+            <div className="shareSocialPopup" onClick={this.toggleSocial}>
+              <span>
+                <a href="#"><i className="fa fa-google-plus"></i></a>
+                <a href="#"><i className="fa fa-twitter"></i></a>
+                <a href="#"><i className="fa fa-facebook"></i></a>
+                <a href="#"><i className="fa fa-envelope-o"></i></a>
+              </span>
+            </div>
+            : null}
+          </CSSTransisitionGroup>
+        </div>
+      )
+    }
   }
-}
 
-module.exports = Storycard;
+  module.exports = Storycard;
